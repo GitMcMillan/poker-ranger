fb9e378781aab00253501452835c5cde9be26959
alexandermcmillan <your-email@domain.com>
2025-02-27 21:47:34 -0800

    grid up

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+const body = document.body;
+const cardBox = document.querySelector(".grid");
+
+for (let i = 0; i < 169; i++) {
+  const theDiv = document.createElement("div");
+  theDiv.textContent = "AA";
+  theDiv.className = "cell";
+
+  cardBox.appendChild(theDiv);
+}
